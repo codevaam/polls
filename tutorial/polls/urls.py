0c9178e88b698983c984f5331517bdf51c9668b4
devamtrivedi <devamtrivedi71@gmail.com>
2019-03-05 01:26:01 +0530

    works

--- tutorial/polls/urls.py
+++ tutorial/polls/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 
 from . import views
 
+# app_name = "polls"
 urlpatterns = [
 	path('',views.index, name='index'),
-	path('<question_id>',views.choice,name='choice'),
+	path('home/',views.home, name='home'),
+	path('<int:question_id>',views.choice),
+	path('<int:question_id>/results/',views.results),
+	path('<int:question_id>/vote/',views.vote),
 ]
--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,9 +1,10 @@
 import datetime
 
-from django.http import HttpResponse
-from django.shortcuts import render,redirect
+from django.http import HttpResponse,HttpResponseRedirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.views.generic import TemplateView
 from django.forms import inlineformset_factory
+from django.urls import reverse
 
 from polls.form import QuestionForm
 from polls.models import Question,Choice
@@ -17,7 +18,7 @@ def choice(request, question_id):
 		if formset.is_valid():
 			formset.save()
 
-		return redirect('choice', question_id = question.id)
+		return redirect(choice, question_id = question.id)
 	formset = ChoiceFormset(instance=question)
 
 	return render(request, 'polls/index.html', {'formset': formset, 'question_text': question.question_text})
@@ -36,6 +37,26 @@ def index(request):
 	if request.method=='GET':
 		form = QuestionForm()
 		return render(request, "polls/create.html", {'form': form})
+
+def vote(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except(KeyError,Choice.DoesNotExist):
+		return render(request,'polls/detail.html',{'question': question, 'error_message': "You didn't select a choice",})
+	else:
+		selected_choice.votes +=1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
+
+def results(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html', {'question':question})
+
+def home(request):
+	latest_question_list=Question.objects.order_by('-pub_date')[:5]
+	context = {'latest_question_list': latest_question_list}
+	return render(request, 'polls/home.html', context)
 # class AddPollsView(TemplateView):
 
 # 	def get(self, request):
